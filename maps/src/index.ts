cd70cf209a42740aeff0d126eb06856e359b43b8
Levi <lhalvorson34@gmail.com>
2021-03-22 12:26:18 -0600

    done typescript

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,16 +1,12 @@
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+import { User } from "./User";
+import { Company } from "./Company";
 
-// const user = new User();
-// console.log(user);
+const user = new User();
+console.log(user);
 
-// const company = new Company();
-// console.log(company);
+const company = new Company();
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const customMap = new CustomMap("map");
+customMap.addMarker(user);
+customMap.addMarker(company);
